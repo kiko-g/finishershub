35e47ed8be32b972d2dd19280913927717bf88be
kiko-g <kikojpgoncalves@gmail.com>
2022-04-13 04:41:26 +0100

    fix: twitch embed parent url

--- src/components/VideoClip.tsx
+++ src/components/VideoClip.tsx
@@ -8,7 +8,11 @@ export const VideoClip = ({ video }: VideoClipProps) => {
   return (
     <div className="h-full w-full rounded-xl shadow">
       <div className="relative h-full w-full">
-        <iframe allowFullScreen className="aspect-video w-full rounded-xl" src={`${video}&parent=localhost`}></iframe>
+        <iframe
+          allowFullScreen
+          className="aspect-video w-full rounded-xl"
+          src={`${video}&parent=${process.env.GATSBY_DOMAIN}`}
+        ></iframe>
       </div>
     </div>
   )
