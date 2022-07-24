0d13074e714e4f1392da9ce50b08688e16790ee2
kiko-g <kikojpgoncalves@gmail.com>
2022-07-24 04:01:35 +0100

    add: registry page

---
+++ src/pages/registry.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../layout'
+import Seo from '../components/Seo'
+
+const RegistryPage = () => (
+  <Layout location="Registry">
+    <Seo title="Registry" />
+  </Layout>
+)
+
+export default RegistryPage
