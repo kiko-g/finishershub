17dfe3e54666807f5ca010ea7c4d399df87c562a
kiko-g <kikojpgoncalves@gmail.com>
2022-09-19 23:05:38 +0300

    update: 404 style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,19 +5,19 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const NotFoundPage = () => (
   <Layout location="Oops">
-    <Seo title="404: Not found" />
-    <div className="flex flex-col items-center justify-center space-y-2">
+    <Seo title="Oops" />
+    <div className="flex flex-col items-start justify-start space-y-2">
+      <div className="font-normal">
+        <h2 className="text-3xl font-extrabold tracking-tight sm:text-5xl">404: Not Found</h2>
+        <p>Not much too see here... You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
       <StaticImage
         src="../../static/images/turtle.jpg"
         alt="Not found display"
         objectFit="cover"
         objectPosition="50% 50%"
-        className="aspect-square rounded-xl shadow"
+        className="aspect-square h-auto lg:h-96 rounded-xl shadow"
       />
-      <div>
-        <h1>404: Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </div>
     </div>
   </Layout>
 )
