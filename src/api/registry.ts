210e11a1fcbb560ac4a05cebc7c13aeb6498133a
kiko-g <kikojpgoncalves@gmail.com>
2023-01-04 18:04:49 +0200

    update: registry api

--- src/api/registry.ts
+++ src/api/registry.ts
@@ -1,38 +1,36 @@
 import axios, { AxiosInstance } from 'axios'
 
-const domainEnv = process.env.GATSBY_BACKEND_DOMAIN || 'https://finishershub-backend.netlify.app'
+const domainEnv = process.env.GATSBY_BACKEND_DOMAIN || 'https://finishershub-backend.vercel.app'
 const domain = domainEnv.slice(-1) === '/' ? domainEnv.slice(0, -1) : domainEnv
 
 const backend: AxiosInstance = axios.create({
-  baseURL: `${domain}/registry`,
+  baseURL: `${domain}/api`,
   headers: {
     Accept: 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   },
 })
 
 const getAllFinishers = (callback: Function) => {
-  backend.get(`/`).then(response => callback(response.data))
+  backend.get(`/registry`).then(response => callback(response.data))
 }
 
 const getFinishers = (id: string, callback: Function) => {
-  backend.get(`/${id}`).then(response => callback(response.data))
+  backend.get(`/registry/${id}`).then(response => callback(response.data))
 }
 
 const incrementFinishers = (id: string, arena: number, callback: Function) => {
-  console.log(`/${id}/${arena}/increment`)
-  backend.put(`/${id}/${arena}/increment`).then(response => callback(response.data))
+  backend.put(`/registry/increment/${id}/${arena}`).then(response => callback(response.data))
 }
 
 const decrementFinishers = (id: string, arena: number, callback: Function) => {
-  console.log(`/${id}/${arena}/decrement`)
-  backend.put(`/${id}/${arena}/decrement`).then(response => callback(response.data))
+  backend.put(`/registry/decrement/${id}/${arena}`).then(response => callback(response.data))
 }
 
 const updatePassword = (id: string, newPassword: string, callback: Function) => {
-  backend.put(`/${id}/password/${newPassword}`).then(response => callback(response.data))
+  backend.put(`/registry/${id}/password/${newPassword}`).then(response => callback(response.data))
 }
 
 const registryApi = {
