b1f2a306e85444f9475d5edc5ba4f94e46f83537
kiko-g <kikojpgoncalvesgmail.com>
2023-02-22 20:59:53 +0000

    remove: bad logs

--- pages/info.tsx
+++ pages/info.tsx
@@ -5,14 +5,6 @@ import classNames from 'classnames'
 type Props = {}
 
 export default function InfoPage({}: Props) {
-  const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/gm, '\n')
-  const GOOGLE_SPREADSHEET_ID = process.env.GOOGLE_SHEET_ID!
-  const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!
-
-  console.log('GOOGLE_PRIVATE_KEY', GOOGLE_PRIVATE_KEY)
-  console.log('GOOGLE_SPREADSHEET_ID', GOOGLE_SPREADSHEET_ID)
-  console.log('GOOGLE_SERVICE_ACCOUNT_EMAIL', GOOGLE_SERVICE_ACCOUNT_EMAIL)
-
   const [headers, setHeaders] = useState<string[]>([])
   const [catalogue, setCatalogue] = useState<(string | number)[][]>([])
 
