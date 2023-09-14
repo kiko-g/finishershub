ea1a934cbfbfb882c1a7fe987e33a92ffc3fd04b
kiko-g <kikojpgoncalves@gmail.com>
2023-09-14 11:49:01 +0100

    update: full access badge

--- components/layout/FullAccessBadge.tsx
+++ components/layout/FullAccessBadge.tsx
@@ -1,16 +1,21 @@
 import React from "react"
 import { CheckBadgeIcon } from "@heroicons/react/24/outline"
+import { DeleteCookiesButton } from "../videos/DeleteCookiesButton"
 
 export function FullAccessBadge() {
   const [folded, setFolded] = React.useState<boolean>(true)
 
   return (
-    <button
-      onClick={() => setFolded(!folded)}
-      className="flex items-center justify-center gap-x-1 rounded border border-teal-600/80 bg-teal-600/50 px-3 py-2 text-white transition-all hover:bg-teal-600 lg:px-2 lg:py-1.5"
-    >
-      <CheckBadgeIcon className="h-5 w-5" />
-      {folded ? null : <span className="whitespace-nowrap text-sm font-normal tracking-tighter">Full Access</span>}
-    </button>
+    <div className="flex items-center gap-2">
+      <button
+        onClick={() => setFolded(!folded)}
+        className="flex items-center justify-center gap-x-1 rounded border border-emerald-500 bg-emerald-500/60 px-3 py-2 text-white transition-all hover:bg-emerald-500 lg:px-2 lg:py-1.5"
+      >
+        <CheckBadgeIcon className="h-5 w-5" />
+        {folded ? null : <span className="whitespace-nowrap text-sm font-normal tracking-tighter">Full Access</span>}
+      </button>
+
+      {folded ? null : <DeleteCookiesButton />}
+    </div>
   )
 }
--- components/videos/DeleteCookiesButton.tsx
+++ components/videos/DeleteCookiesButton.tsx
@@ -1,14 +1,14 @@
 import React from "react"
+import classNames from "classnames"
 import { clearCache } from "../../utils/storage"
 import { FireIcon } from "@heroicons/react/24/outline"
-import classNames from "classnames"
 import { getButtonSizeClassNames } from "../../utils"
 
 type Props = {
   size?: "xs" | "sm" | "md" | "lg" | "xl"
 }
 
-export function DeleteCookiesButton({ size = "sm" }: Props) {
+export function DeleteCookiesButton({ size = "xs" }: Props) {
   function deleteData() {
     if (typeof window === "undefined") return
 
@@ -19,19 +19,12 @@ export function DeleteCookiesButton({ size = "sm" }: Props) {
   }
 
   return (
-    <div className="flex items-end justify-center space-x-2">
-      <button
-        onClick={deleteData}
-        title="Clear all cookies (helps if page does not load)"
-        className="transition hover:opacity-80"
-      >
-        <FireIcon
-          className={classNames(
-            getButtonSizeClassNames(size),
-            "fill-transparent text-orange-500 dark:fill-orange-500/10 dark:text-orange-500",
-          )}
-        />
-      </button>
-    </div>
+    <button
+      onClick={deleteData}
+      title="Clear all cookies and local storage"
+      className="inline-flex items-center justify-center gap-x-1 rounded border border-orange-500 bg-orange-500/70 px-1.5 py-1.5 text-center text-xs text-white transition hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-50 dark:border-orange-500 dark:bg-orange-500/50 lg:px-2 lg:py-1.5 lg:text-sm"
+    >
+      <FireIcon className={classNames(getButtonSizeClassNames(size))} />
+    </button>
   )
 }
